8d8fa90246d143368b39677f7533c600660f192d
xul <1119059692@qq.com>
2020-02-09 17:13:05 +0800

    init

---
+++ src/test/java/com/bby/jumbo/JumboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bby.jumbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
